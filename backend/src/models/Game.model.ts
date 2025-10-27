1e992e1eaed0c96096da0bd9c1eafc9daec10e68
Murphy <murphytsirofozafy@gmail.com>
2025-10-27 14:43:06 +0300

    resolve TypeScript (TS2532 et TS18047) error in prod

--- backend/src/models/Game.model.ts
+++ backend/src/models/Game.model.ts
@@ -3,9 +3,9 @@ import Frame from "./Frame.model";
 import Lancer from "./Lancer.model";
 
 export default class Game {
-    indiceActualFrame:number;
-    indiceActualLancer:number;
-    frames:Frame[] = [];
+    indiceActualFrame: number;
+    indiceActualLancer: number;
+    frames: Frame[] = [];
     bonusLancers: BonusLancer[] = [];
     scoreFinal: number;
     isFinished: boolean;
@@ -21,17 +21,18 @@ export default class Game {
         }
     }
 
-    toString():string {
-        let res:string = "";
-        
+    toString(): string {
+        let res: string = "";
+
         this.frames.forEach((frame, index) => {
-            res += ( index < 5 ? frame.toString() : frame.toString(true))+": "+frame.totalActuel+"  ";
-        }); 
+            const totalAAfficher = frame.totalAAfficher ?? frame.totalActuel;
+            res += (index < 5 ? frame.toString() : frame.toString(true)) + ": " + totalAAfficher + "  ";
+        });
 
         return res;
-    }  
+    }
 
-    addFrame (frame: Frame) {
+    addFrame(frame: Frame) {
         if (this.frames.length < 6) {
             this.frames.push(frame);
         } else {
@@ -39,103 +40,107 @@ export default class Game {
         }
     }
 
-    addBonusLancer (bonusLancer: BonusLancer) {
+    addBonusLancer(bonusLancer: BonusLancer) {
         this.bonusLancers.push(bonusLancer);
     }
 
-    removeAllBonusLancerWithNbrLancerZero () {
-        const zeroLancer: BonusLancer = this.bonusLancers.find(
-            bonusLancer => bonusLancer.nbrLancer === 0
-        );
+    removeAllBonusLancerWithNbrLancerZero() {
+        const zeroLancer = this.bonusLancers.find(bonusLancer => bonusLancer.nbrLancer === 0);
 
-        if (zeroLancer !== undefined) {
-            if (zeroLancer.indiceFrame == 0) {
-                this.frames[zeroLancer.indiceFrame].totalAAfficher = this.frames[zeroLancer.indiceFrame].totalActuel; 
-            } else {
-                this.frames[zeroLancer.indiceFrame].totalAAfficher = this.frames[zeroLancer.indiceFrame-1].totalAAfficher + this.frames[zeroLancer.indiceFrame].totalActuel;    
+        if (zeroLancer) {
+            const frame = this.frames[zeroLancer.indiceFrame];
+            if (frame) {
+                frame.totalAAfficher = zeroLancer.indiceFrame === 0
+                    ? frame.totalActuel
+                    : (this.frames[zeroLancer.indiceFrame - 1]?.totalAAfficher ?? 0) + frame.totalActuel;
+                this.scoreFinal = frame.totalAAfficher;
             }
-            this.scoreFinal = this.frames[zeroLancer.indiceFrame].totalAAfficher;
         }
 
-        this.bonusLancers = this.bonusLancers.filter(bonusLancer => bonusLancer.nbrLancer !== 0);          
+        this.bonusLancers = this.bonusLancers.filter(bonusLancer => bonusLancer.nbrLancer !== 0);
     }
 
     addPointToFrameInBonusLancer(point: number) {
-        this.bonusLancers.forEach((bonusLancer, index) => {
-            this.frames[bonusLancer.indiceFrame]!.totalActuel += point;
+        this.bonusLancers.forEach((bonusLancer) => {
+            const frame = this.frames[bonusLancer.indiceFrame];
+            if (!frame) return;
+            frame.totalActuel += point;
             bonusLancer.decrementerNbrLancer();
         });
     }
 
     setFinalScoreFromThrowPoints() {
-        this.scoreFinal = this.frames.reduce((acc, frame) => acc + frame.totalActuel, 0);
+        this.scoreFinal = this.frames.reduce((acc, frame) => acc + (frame.totalActuel ?? 0), 0);
     }
 
-    addNewPoint (point: number) {
-        if (!this.isFinished) {
+    addNewPoint(point: number) {
+        if (this.isFinished) {
+            throw new Error("Le jeu est déjà terminé !");
+        }
 
-            let actualFrame: Frame = this.frames[this.indiceActualFrame]!;
-            let actualLancer: Lancer = actualFrame.lancers[this.indiceActualLancer]!;
-            let copyIndiceActualFrame = this.indiceActualFrame;
+        const actualFrame = this.frames[this.indiceActualFrame];
+        if (!actualFrame) return;
 
-            const sommeLancersAvant = actualFrame.lancers.slice(0, this.indiceActualLancer).reduce((acc: number, lancer: Lancer) => acc + lancer.point, 0);
+        const actualLancer = actualFrame.lancers[this.indiceActualLancer];
+        if (!actualLancer) return;
 
-            const pointToAdd = (point === 15 && this.indiceActualLancer > 0 && this.indiceActualFrame < 5) ? 15 - sommeLancersAvant : point;
-    
-            actualLancer.point = point;
-            actualFrame.totalActuel += (this.indiceActualFrame < 5) ? pointToAdd : 0; 
-    
-            this.addPointToFrameInBonusLancer(pointToAdd);
-    
-            if (point == 15) {
-    
-                if (this.indiceActualLancer == 0) {
-                    actualLancer.isStrike = true;
-                    if (this.indiceActualFrame < 5 ) {
-                        this.addBonusLancer(new BonusLancer(this.indiceActualFrame, 3));
-                    }
-                } else {
-                    actualLancer.isSpare = true;
-                    if (this.indiceActualFrame < 5 ) {
-                        this.addBonusLancer(new BonusLancer(this.indiceActualFrame, 2));
-                    }
+        const copyIndiceActualFrame = this.indiceActualFrame;
+
+        const sommeLancersAvant = actualFrame.lancers
+            .slice(0, this.indiceActualLancer)
+            .reduce((acc: number, lancer: Lancer) => acc + (lancer.point ?? 0), 0);
+
+        const pointToAdd = (point === 15 && this.indiceActualLancer > 0 && this.indiceActualFrame < 5)
+            ? 15 - sommeLancersAvant
+            : point;
+
+        actualLancer.point = point;
+        actualFrame.totalActuel += (this.indiceActualFrame < 5 ? pointToAdd : 0);
+
+        this.addPointToFrameInBonusLancer(pointToAdd);
+
+        if (point === 15) {
+            if (this.indiceActualLancer === 0) {
+                actualLancer.isStrike = true;
+                if (this.indiceActualFrame < 5) {
+                    this.addBonusLancer(new BonusLancer(this.indiceActualFrame, 3));
                 }
-    
-                this.indiceActualLancer = (this.indiceActualFrame >= 5) ? (this.indiceActualLancer + 1) % 3 : 0;
-                this.indiceActualFrame = (this.indiceActualFrame >= 5) ? this.indiceActualFrame : this.indiceActualFrame + 1;
-    
             } else {
-                this.indiceActualLancer = ( this.indiceActualLancer + 1 ) % 3;
-                this.indiceActualFrame = (this.indiceActualLancer == 0) ? this.indiceActualFrame + 1  : this.indiceActualFrame;
-            }
-    
-            this.removeAllBonusLancerWithNbrLancerZero();
-            if (this.indiceActualFrame != copyIndiceActualFrame)  {
-                if (!this.bonusLancers.some(bonusLancer => bonusLancer.indiceFrame === copyIndiceActualFrame)) {
-                    if (copyIndiceActualFrame == 0) {
-                        this.frames[copyIndiceActualFrame].totalAAfficher = this.frames[copyIndiceActualFrame].totalActuel; 
-                    } else {
-                        this.frames[copyIndiceActualFrame].totalAAfficher = this.frames[copyIndiceActualFrame-1].totalAAfficher + this.frames[copyIndiceActualFrame].totalActuel;    
-                    }
-                    this.scoreFinal = this.frames[copyIndiceActualFrame].totalAAfficher;
+                actualLancer.isSpare = true;
+                if (this.indiceActualFrame < 5) {
+                    this.addBonusLancer(new BonusLancer(this.indiceActualFrame, 2));
                 }
             }
 
-            console.log(this.toString());
-            console.log("totalScore :", this.scoreFinal);
-            console.log("-------------------------");
-            
-            
-    
-            if (this.indiceActualFrame >= 5 && this.bonusLancers.length == 0) {
-                this.isFinished = true;
-                this.setFinalScoreFromThrowPoints();
-            }
+            this.indiceActualLancer = (this.indiceActualFrame >= 5) ? (this.indiceActualLancer + 1) % 3 : 0;
+            this.indiceActualFrame = (this.indiceActualFrame >= 5) ? this.indiceActualFrame : this.indiceActualFrame + 1;
 
         } else {
-            throw new Error("Le jeu est dejà terminé !");
+            this.indiceActualLancer = (this.indiceActualLancer + 1) % 3;
+            this.indiceActualFrame = (this.indiceActualLancer === 0) ? this.indiceActualFrame + 1 : this.indiceActualFrame;
+        }
+
+        this.removeAllBonusLancerWithNbrLancerZero();
+
+        if (this.indiceActualFrame !== copyIndiceActualFrame) {
+            if (!this.bonusLancers.some(bonusLancer => bonusLancer.indiceFrame === copyIndiceActualFrame)) {
+                const frame = this.frames[copyIndiceActualFrame];
+                if (frame) {
+                    frame.totalAAfficher = copyIndiceActualFrame === 0
+                        ? frame.totalActuel
+                        : (this.frames[copyIndiceActualFrame - 1]?.totalAAfficher ?? 0) + frame.totalActuel;
+                    this.scoreFinal = frame.totalAAfficher;
+                }
+            }
+        }
+
+        console.log(this.toString());
+        console.log("totalScore :", this.scoreFinal);
+        console.log("-------------------------");
+
+        if (this.indiceActualFrame >= 5 && this.bonusLancers.length === 0) {
+            this.isFinished = true;
+            this.setFinalScoreFromThrowPoints();
         }
     }
-
-
 }
